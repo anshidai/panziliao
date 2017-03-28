de244833a0be9156edf80ba212046af9ffe37527
libaoan <287639598@qq.com>
2017-03-28 10:15:36 +0800

    调整

--- application/Console/Controller/resetStatusController.class.php
+++ application/Console/Controller/resetStatusController.class.php
@@ -4,19 +4,44 @@ namespace Console\Controller;
 
 use Think\Controller;
 
+/**
+* 执行脚本 php index.php Console/ResetStatus/xxx
+*/
 class ResetStatusController extends Controller
 {
     protected function _initialize()
     {
         
     }
-    
-    public function resetUser()
-    {
-        $model = D('ResUser');
-        
-    }
-    
+	
+	/**
+	* 定时审核通过用户和分享信息 status=2
+	*/
+    public function updateRestStatus()
+	{
+		$userids = array();
+		$pagesize = 60; //用户每次审核通过数量
+		
+		$model = D('ResUser');
+		$list = $model->field('id,status')->where(array('status'=>1))->order('id asc')->limit($pagesize)->select();
+		if($list) {
+			foreach($list as $val) {
+				$userids[$val['id']] = $val['id'];
+				$model->where(array('id'=>$val['id']))->save(array('status'=>2));
+			}
+		}
+		
+		if(!empty($userids)) {
+			$model = D('ResDetail');
+			foreach($userids as $uid) {
+				$model->where(array('res_user_id'=>$uid))->save(array('status'=>2));
+			}
+		}
+	}
+	
+    /**
+	* 批量更新字段值
+	*/
     public function resetDetail()
     {
         $resDetailModel = D('ResDetail');
